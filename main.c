133e644dc40c256aa05da3dbc2696c94ab54ee4a
Adrien D <etu48751@henallux.be>
2025-10-29 10:51:02 +0100

    Suite
    Patch de lisibilité + Calcul de la période + Génération de la suite de nombres pseudo-aléatoires /!\ Ne va pas plus loin que m /!\

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 
-int determinerPGCD(int nombre1, int nombre2);
+int calculerPGCD(int nombre1, int nombre2);
 bool determinerEstPremier(int nombre);
+int calculerPeriode(int x0, int a, int c, int m);
+void genererSuite(int x0, int a, int c, int m);
 
 int main(void) {
     /* Declaration des coefficients */
 
-    int x0 = 0;
-    int a = 0;
-    int c = 0;
-    int m = 0;
+    int x0, a, c, m;
 
     /* Initialisation des valeurs des coefficients */
 
@@ -63,12 +63,16 @@ int main(void) {
 
     bool estOK = true;
 
-    if (determinerPGCD(c, m) != 1) {
+    // Hypothese 1
+
+    if (calculerPGCD(c, m) != 1) {
         printf("[HYPOTHESE 1] : c et m ne sont pas premiers entre eux\n\n");
         estOK = false;
     } else {
         printf("[HYPOTHESE 1] : c et m sont premiers entre eux\n\n");
     }
+
+    // Hypothese 2
 
     for (int p = 2; p <= m; p++) {
         if (determinerEstPremier(p) && (m % p) == 0) {
@@ -83,6 +87,8 @@ int main(void) {
         printf("[HYPOTHESE 2] : (a - 1) est multiple de tous les facteurs premiers de m\n\n");
     }
 
+    // Hypothese 3
+
     if (m % 4 == 0) {
         if ((a - 1) % 4 != 0) {
             printf("[HYPOTHESE 3] : m est multiple de 4 mais (a-1) ne l'est pas\n\n");
@@ -94,10 +100,42 @@ int main(void) {
         printf("[HYPOTHESE 3] : m n'est pas multiple de 4\n\n");
     }
 
+    // Conclusion
+
+    if (estOK) {
+        printf("Les coefficients remplissent les conditions du theoreme de Hull-Dobell\n\n");
+    } else {
+        printf("Les coefficients ne remplissent pas toutes les conditions du theoreme du Hull-Dobell\n\n");
+    }
+
+    /* Calcul de la periode de la suite */
+
+    printf("-- CALCUL DE LA PERIODE DE LA SUITE --\n\n");
+
+    int periode;
+
+    if (estOK) {
+        periode = m;
+    } else {
+        periode = calculerPeriode(x0, a, c, m);
+    }
+
+    if (periode == -1) {
+        printf("[ERREUR] : l'allocation de la memoire a echouee\n\n");
+    } else {
+        printf("La periode de la suite est de %d\n\n", periode);
+    }
+
+    /* Generation de la suite de nombres pseudo-aleatoires */
+
+    printf("\n-- GENERATION DE LA SUITE PSEUDO-ALEATOIRE --\n\n");
+
+    genererSuite(x0, a, c, m);
+
     return 0;
 }
 
-int determinerPGCD(int nombre1, int nombre2) {
+int calculerPGCD(int nombre1, int nombre2) {
     int t;
 
     while (nombre2 != 0) {
@@ -110,11 +148,58 @@ int determinerPGCD(int nombre1, int nombre2) {
 }
 
 bool determinerEstPremier(int nombre) {
-    if (nombre < 2) return false;
+    if (nombre < 2) {
+        return false;
+    }
 
     for (int i = 2; i * i <= nombre; i++) {
-        if (nombre % i == 0) return false;
+        if (nombre % i == 0) {
+            return false;
+        }
     }
 
     return true;
+}
+
+int calculerPeriode(int x0, int a, int c, int m) {
+    int periode;
+    int x = x0;
+    int etape = 0;
+
+    int* suite = malloc(m * sizeof(int));
+
+    if (suite == NULL) {
+        return -1;
+    }
+
+    for (int i = 0; i < m; i++) {
+        suite[i] = -1;
+    }
+
+    while (suite[x] == -1) {
+        suite[x] = etape;
+        x = (a * x + c) % m;
+        etape++;
+    }
+
+    periode = etape - suite[x];
+
+    free(suite);
+
+    return periode;
+}
+
+void genererSuite(int x0, int a, int c, int m) {
+    int x = x0;
+
+    printf("%d", x);
+
+    for (int i = 1; i < m; i++) {
+        x = (a * x + c) % m;
+
+        printf(" ");
+        printf("%d", x);
+    }
+
+    printf("\n\n");
 }
